722fd05f08507b6e9e75a5fe9bd61c75237f2b4c
aifrosals <a.ifrosals@gmail.com>
2019-10-29 12:19:33 +0500

    assets and theme

---
+++ assets/theme/palette.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Palette {
+
+  static Color primaryColor = Colors.white;
+  static Color accentColor =  Color(0xff4fc3f7);
+  static Color secondaryColor = Colors.black;
+
+}
