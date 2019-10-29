c3e1b668033a0459829f87cea46ac0cf2647bbe1
aifrosals <a.ifrosals@gmail.com>
2019-10-29 13:18:47 +0500

    assets and theme

---
+++ lib/pages/exercise.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class exercise extends StatefulWidget {
+  @override
+  _exerciseState createState() => _exerciseState();
+}
+
+class _exerciseState extends State<exercise> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+
+          )
+        ],
+      ),
+    );
+  }
+}
