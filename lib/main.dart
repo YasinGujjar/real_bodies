e3c55d6fc53461fe1d9d0cd89529672b72e44f46
aifrosals <a.ifrosals@gmail.com>
2019-10-29 12:36:13 +0500

    assets and theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:real_bodies/theme/palette.dart';
+
 
 void main() => runApp(MyApp());
 
@@ -18,7 +20,7 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Palette.accentColor,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
--- lib/theme/palette.dart
+++ lib/theme/palette.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 
-class Palette {
+class Palette{
 
   static Color primaryColor = Colors.white;
   static Color accentColor =  Color(0xff4fc3f7);
   static Color secondaryColor = Colors.black;
+
 
 }
