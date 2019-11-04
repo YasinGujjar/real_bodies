63813f046eadb3379f94fde1879d5cb5d1bdaa9a
aifrosals <a.ifrosals@gmail.com>
2019-11-04 12:49:53 +0500

    FoodDiaryToday app Bar

--- lib/pages/food_diary_today.dart
+++ lib/pages/food_diary_today.dart
@@ -8,13 +8,29 @@ class FoodDiaryToday extends StatefulWidget {
 class _FoodDiaryTodayState extends State<FoodDiaryToday> {
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+//    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+//        statusBarColor: Colors.green
+//    ));
     return Scaffold(
       appBar: AppBar(
-        title: Column(
-          children: <Widget>[Text('Food Diary'), Text('< Thu,Feb 4 >')],
-        ),
-
+        title: Center(child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            IconButton(icon: Icon(Icons.arrow_back_ios),
+                color: Colors.white,
+                onPressed: ()=>{}),
+            Text('Today', style: TextStyle(color: Colors.white),),
+            IconButton(icon: Icon(Icons.arrow_forward_ios),
+                color: Colors.white,
+                onPressed: ()=>{}),
+          ],
+        )),
       ),
+    body:
+        Container()
+
     );
   }
 }
