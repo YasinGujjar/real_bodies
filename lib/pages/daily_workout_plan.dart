02939d417647e302cf63f4ed598ff101bcbdb2eb
aifrosals <a.ifrosals@gmail.com>
2019-11-10 01:05:27 +0500

    Merge branch 'master' of https://github.com/YasinGujjar/real_bodies
    # Conflicts:
    #	lib/main.dart

--- lib/pages/daily_workout_plan.dart
+++ lib/pages/daily_workout_plan.dart
@@ -16,11 +16,11 @@ class _DailyWorkOutPlanState extends State<DailyWorkOutPlan> {
   Widget build(BuildContext context) {
 
    double width = MediaQuery.of(context).size.width;
-  //  double height = MediaQuery.of(context).size.height;
+    double height = MediaQuery.of(context).size.height;
     return SafeArea(
    child: Theme(
      data: Theme.of(context).copyWith(
-       canvasColor: Colors.transparent
+       canvasColor: Colors.transparent,
      ),
      child: Material(
 
@@ -28,38 +28,45 @@ class _DailyWorkOutPlanState extends State<DailyWorkOutPlan> {
      //   child:    Container(
          // color: Colors.transparent,
          // color: Colors.blue,
-          child: CustomScrollView(
+          child:
+             Column(
+               children: <Widget>[
+                 Container(
+                   height: height * 0.85,
+
+                   child: CustomScrollView(
 
 
-            slivers:[
+                      slivers:[
 
-              SliverPersistentHeader(
-                delegate: MySliverAppBar(expandedHeight: 200),
-                pinned: true,
-              ),
-
-
+                        SliverPersistentHeader(
+                          delegate: MySliverAppBar(expandedHeight: 200),
+                          pinned: true,
+                        ),
 
 
-              SliverPersistentHeader(
-                delegate: MiddleSliverAppBar(expandedHeight: 70, title:'30 minutes'),
-                //delegate: ,
-                floating: false,
-               pinned: true,
-              ),
-              SliverPersistentHeader(
-                delegate: MiddleSecondSliverAppBar(expandedHeight: 28),
-                //delegate: ,
-                pinned: true,
-                floating: false,
-              ),
 
-          SliverList(
-              delegate: SliverChildListDelegate(
-                [
-               Stack(
-                 children: <Widget>[
-                  Container(
+
+                        SliverPersistentHeader(
+                          delegate: MiddleSliverAppBar(expandedHeight: 80, title:'30 minutes'),
+                          //delegate: ,
+                          floating: false,
+                         pinned: true,
+                        ),
+//                    SliverPersistentHeader(
+//                      delegate: MiddleSecondSliverAppBar(expandedHeight: 28),
+//                      //delegate: ,
+//                      pinned: true,
+//                    //  floating: false,
+//
+//                    ),
+
+                    SliverList(
+
+                        delegate: SliverChildListDelegate(
+                          [
+
+                            Container(
 
 //                 decoration: BoxDecoration(
 //                   color: Palette.mainPurple,
@@ -67,126 +74,185 @@ class _DailyWorkOutPlanState extends State<DailyWorkOutPlan> {
 //
 //                 ),
 
-                  child:
+                            child:
 
-                    Column(
-                     children: <Widget>[
-//                     SizedBox(
-//                       height: height * 0.10,
-//                     ),
+                              Column(
+                               children: <Widget>[
+                     SizedBox(
+                       height: 5,
+                     ),
 
 
-                       Container(
-                        // height: height - height *0.20,
-                         color: Colors.white,
-                         height: 20,
+                                 Container(
+                                  // height: height - height *0.20,
+                                  // color: Colors.white,
+                                //   height: 20,
 //                       decoration: BoxDecoration(
 //                         color: Colors.white,
 //                         borderRadius: BorderRadius.only(topLeft: Radius.elliptical(70,40),topRight: Radius.elliptical(70,40)),
 //
 //                       ),
-                       ),
+                                 ),
 
-                       Container(
-                         width: width,
+                                 Container(
+                                   width: width,
 
-                         color: Colors.white,
-                         child: Column(
-                           children: <Widget>[
-                           Card(
-                           elevation: 8.0,
-                           margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
-                           child: Container(
-                             decoration: BoxDecoration(color: Color.fromRGBO(64, 75, 96, .9)),
-                             child: makeListTile,
-                           ),
-                         ),
-                           ],
-                         ),
-                       ),
-                       Container(
-                         width: width,
+                                   color: Colors.white,
+                                   child: Column(
+                                     children: <Widget>[
+                                     Card(
+                                     elevation: 0.0,
+                                     margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
+                                     child: Container(
+                                       decoration: BoxDecoration(//color: Color.fromRGBO(64, 75, 96, .9),
+                                       border: Border.all(color: Colors.grey, width: 2.5),
+                                       borderRadius: BorderRadius.circular(25.0),),
+                                       child: makeListTile2,
+                                     ),
+                                   ),
+                                     ],
+                                   ),
+                                 ),
+                                 Container(
+                                   width: width,
 
-                         color: Colors.white,
-                         child: Column(
-                           children: <Widget>[
-                             Card(
-                               elevation: 8.0,
-                               margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
-                               child: Container(
-                                 decoration: BoxDecoration(color: Color.fromRGBO(64, 75, 96, .9)),
-                                 child: makeListTile,
-                               ),
+                                   color: Colors.white,
+                                   child: Column(
+                                     children: <Widget>[
+                                       Card(
+                                         elevation: 0.0,
+                                         margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
+                                         child: Container(
+                                           decoration: BoxDecoration(color: Color.fromRGBO(64, 75, 96, .9),
+                                               borderRadius: BorderRadius.circular(25.0),
+
+                                         ),
+                                           child: makeListTile,
+                                         ),
+                                       ),
+                                     ],
+                                   ),
+                                 ),
+                                 Container(
+                                   width: width,
+
+                                   color: Colors.white,
+                                   child: Column(
+                                     children: <Widget>[
+                                       Card(
+                                         elevation: 0.0,
+                                         margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
+                                         child: Container(
+                                           decoration: BoxDecoration(color: Color.fromRGBO(64, 75, 96, .9)),
+                                           child: makeListTile,
+                                         ),
+                                       ),
+                                     ],
+                                   ),
+                                 ),
+                                 Container(
+                                   width: width,
+
+                                   color: Colors.white,
+                                   child: Column(
+                                     children: <Widget>[
+                                       Card(
+                                         elevation: 0.0,
+                                         margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
+                                         child: Container(
+                                           decoration: BoxDecoration(color: Color.fromRGBO(64, 75, 96, .9)),
+                                           child: makeListTile,
+                                         ),
+                                       ),
+                                     ],
+                                   ),
+                                 ),
+                                 Container(
+                                   width: width,
+
+                                   color: Colors.white,
+                                   child: Column(
+                                     children: <Widget>[
+                                       Card(
+                                         elevation: 0.0,
+                                         margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
+                                         child: Container(
+                                           decoration: BoxDecoration(color: Color.fromRGBO(64, 75, 96, .9)),
+                                           child: makeListTile,
+                                         ),
+                                       ),
+                                     ],
+                                   ),
+                                 ),
+                                 Container(
+                                   width: width,
+
+                                   color: Colors.white,
+                                   child: Column(
+                                     children: <Widget>[
+                                       Card(
+                                         elevation: 0.0,
+                                         margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
+                                         child: Container(
+                                           decoration: BoxDecoration(color: Color.fromRGBO(64, 75, 96, .9)),
+                                           child: makeListTile,
+                                         ),
+                                       ),
+                                     ],
+                                   ),
+                                 ),
+
+
+
+                               ],
                              ),
-                           ],
-                         ),
-                       ),
-                       Container(
-                         width: width,
-
-                         color: Colors.white,
-                         child: Column(
-                           children: <Widget>[
-                             Card(
-                               elevation: 8.0,
-                               margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
-                               child: Container(
-                                 decoration: BoxDecoration(color: Color.fromRGBO(64, 75, 96, .9)),
-                                 child: makeListTile,
-                               ),
-                             ),
-                           ],
-                         ),
-                       ),
-                       Container(
-                         width: width,
-
-                         color: Colors.white,
-                         child: Column(
-                           children: <Widget>[
-                             Card(
-                               elevation: 8.0,
-                               margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
-                               child: Container(
-                                 decoration: BoxDecoration(color: Color.fromRGBO(64, 75, 96, .9)),
-                                 child: makeListTile,
-                               ),
-                             ),
-                           ],
-                         ),
-                       ),
-                       Container(
-                         width: width,
-
-                         color: Colors.white,
-                         child: Column(
-                           children: <Widget>[
-                             Card(
-                               elevation: 8.0,
-                               margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
-                               child: Container(
-                                 decoration: BoxDecoration(color: Color.fromRGBO(64, 75, 96, .9)),
-                                 child: makeListTile,
-                               ),
-                             ),
-                           ],
-                         ),
-                       ),
+                            ),
 
 
-                     ],
-                   ),
-                  ),
-        ],
-               ),
-              ],
-              ),
-          ),
+                        ],
+                        ),
+                    ),
 
 ],
 
 
-          ),
+                    ),
+                 ),
+                 Container(
+                   height: height * 0.11,
+                   color: Colors.white,
+                   child: Center(
+                     child:  Container(
+                       height: 50,
+                       width: width * 0.80,
+                       child: FlatButton(
+                         shape: new RoundedRectangleBorder(
+                             borderRadius: new BorderRadius.circular(30.0),
+                         ),
+                         onPressed: () {},
+                         color: Palette.mainPurple,
+                         textColor: Colors.white,
+                         child: Text("Start".toUpperCase(),
+                             style: TextStyle(fontSize: 14,
+                             fontWeight: FontWeight.bold)),
+                       ),
+                     ),
+                   ),
+                 ),
+               ],
+             ),
+
+
+//            floatingActionButton: FloatingActionButton(
+//              //heroTag: "btn",
+//              onPressed: (){
+//
+//              },
+//              tooltip: 'Increment',
+//              child: Icon(Icons.add),
+//            ), // This trailing comma makes auto-formatting nicer for build methods.
+
+
        // ),
      ),
    ),
@@ -202,7 +268,8 @@ final makeListTile = ListTile(
       decoration: new BoxDecoration(
           border: new Border(
               right: new BorderSide(width: 1.0, color: Colors.white24))),
-      child: Icon(Icons.autorenew, color: Colors.white),
+    //  child: Icon(Icons.autorenew, color: Colors.white),
+      child: ClipRRect(borderRadius: BorderRadius.circular(15.0) ,child: Image.asset('assets/images/fit.jpg'))
     ),
     title: Text(
       "Introduction to Driving",
@@ -218,3 +285,28 @@ final makeListTile = ListTile(
     ),
     trailing:
     Icon(Icons.keyboard_arrow_right, color: Colors.white, size: 30.0));
+
+final makeListTile2 = ListTile(
+    contentPadding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+    leading: Container(
+        padding: EdgeInsets.only(right: 12.0),
+        decoration: new BoxDecoration(
+            border: new Border(
+                right: new BorderSide(width: 1.0, color: Colors.grey))),
+        //  child: Icon(Icons.autorenew, color: Colors.white),
+        child: ClipRRect(borderRadius: BorderRadius.circular(15.0) ,child: Image.asset('assets/images/fit.jpg'))
+    ),
+    title: Text(
+      "Introduction to Driving",
+      style: TextStyle( fontWeight: FontWeight.bold),
+    ),
+    // subtitle: Text("Intermediate", style: TextStyle(color: Colors.white)),
+
+    subtitle: Row(
+      children: <Widget>[
+        Icon(Icons.linear_scale, color: Colors.blue),
+        Text(" Intermediate", style: TextStyle(color: Colors.black))
+      ],
+    ),
+    trailing:
+    Icon(Icons.keyboard_arrow_right, color: Colors.black, size: 30.0));
--- lib/ui/widgets/middle_slivr_bar.dart
+++ lib/ui/widgets/middle_slivr_bar.dart
@@ -29,39 +29,76 @@ class MiddleSliverAppBar extends SliverPersistentHeaderDelegate {
         borderRadius: BorderRadius.only(topLeft: Radius.elliptical(70,40),topRight: Radius.elliptical(70,40)),
 
       ),
-        child: Center(
-          child: Text(title,style: TextStyle(
-            color: Colors.white
-          ),),
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              flex: 2,
+                child: Center(child: Text(title, style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold
+                ),),),),
+
+            Expanded(
+              flex: 1,
+              child: Container(
+                width: MediaQuery.of(context).size.width ,
+
+
+                decoration: BoxDecoration(
+
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(topLeft: Radius.elliptical(70,40),topRight: Radius.elliptical(70,40)),
+
+                ),
+                child: Padding(
+                  padding: EdgeInsets.only(left: 17.0),
+                  child: Align(
+                    alignment: Alignment.bottomLeft,
+                      child: Text('Excercises',style: TextStyle(
+                        fontWeight: FontWeight.bold
+                      ),),),
+                ),
+              ),
+            )
+          ],
         ),
 
     ),
     ),
 
 // SizedBox(height: 200,),
-// Positioned(
-//          top: (expandedHeight - shrinkOffset) * 0.85  ,
-//         // left: MediaQuery.of(context).size.width / 4,
+ Positioned(
+          top: expandedHeight * 0.97,//(expandedHeight - shrinkOffset)  ,
+         // left: MediaQuery.of(context).size.width / 4,
 //          child: Opacity(
 //            opacity: ((shrinkOffset >40)? 0 :1 - (shrinkOffset   / expandedHeight)  ),
-//
-////            child: Card(
-////              elevation: 10,
-////              child: SizedBox(
-////                height:  expandedHeight,
-////                width:  MediaQuery.of(context).size.width / 2,
-////                child: FlutterLogo(),
-////              ),
-//    child: Container(
-//      height: 20,
-//    width: MediaQuery.of(context).size.width ,
-//    color: Colors.white,
-//    ),
-//
-//
-//        ),
-//        ),
-//
+
+//            child: Card(
+//              elevation: 10,
+//              child: SizedBox(
+//                height:  expandedHeight,
+//                width:  MediaQuery.of(context).size.width / 2,
+//                child: FlutterLogo(),
+//              ),
+    child: Container(
+      height: 10,
+    width: MediaQuery.of(context).size.width ,
+    color: Colors.white,
+      child: Align(
+        alignment: Alignment.center,
+        child: Container(
+          height: 2.5,
+          width: MediaQuery.of(context).size.width * 0.95,
+          color: Palette.mainPurple,
+        ),
+      ),
+
+    ),
+
+
+       // ),
+        ),
+
 //           Container(
 //             height: 20,
 //            //color: Colors.black,
@@ -82,7 +119,7 @@ class MiddleSliverAppBar extends SliverPersistentHeaderDelegate {
   double get maxExtent => expandedHeight;
 
   @override
-  double get minExtent => 70;
+  double get minExtent => 80;
 
   @override
   bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) => true;
@@ -99,33 +136,39 @@ class MiddleSecondSliverAppBar extends SliverPersistentHeaderDelegate {
     print('shrink offset $shrinkOffset');
 
     return
-      Stack(
-         fit: StackFit.expand,
-        overflow: Overflow.visible,
-        children: [
 
-          Container(
-            color: Palette.mainPurple,
-            child: Container(
-              //color: Colors.black,
+         Stack(
+           fit: StackFit.expand,
+          overflow: Overflow.visible,
+          children: [
 
-              decoration: BoxDecoration(
 
-                color: Colors.white,
-                borderRadius: BorderRadius.only(topLeft: Radius.elliptical(70,40),topRight: Radius.elliptical(70,40)),
+               Container(
+                color:  Palette.mainPurple,
+
+                  child: Container(
+                    //color: Colors.black,
+
+                    decoration: BoxDecoration(
+
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(topLeft: Radius.elliptical(70,40),topRight: Radius.elliptical(70,40)),
+
+                    ),
+
+
+                  ),
 
               ),
 
-            ),
-          ),
 
 
-
-          Positioned(
-            top: (expandedHeight - shrinkOffset)  * 0.89  ,
-            // left: MediaQuery.of(context).size.width / 4,
-            child: Opacity(
-              opacity: ((shrinkOffset >5)? 0 :1 - (shrinkOffset   / expandedHeight)  ),
+//
+            Positioned(
+              top: (expandedHeight - shrinkOffset) ,// * 0.89  ,
+              // left: MediaQuery.of(context).size.width / 4,
+              child: Opacity(
+                opacity: ((shrinkOffset >5)? 0 :1 - (shrinkOffset   / expandedHeight)  ),
 
 //            child: Card(
 //              elevation: 10,
@@ -134,31 +177,32 @@ class MiddleSecondSliverAppBar extends SliverPersistentHeaderDelegate {
 //                width:  MediaQuery.of(context).size.width / 2,
 //                child: FlutterLogo(),
 //              ),
-              child: Container(
-                height: 10,
-                width: MediaQuery.of(context).size.width ,
-                color: Colors.white,
+                child: Container(
+                  height: 10,
+                  width: MediaQuery.of(context).size.width ,
+                  color: Colors.white,
+                ),
+
+
               ),
-
-
             ),
-          ),
 
-// SizedBox(height: 200,),
-//
-//           Container(
-//             height: 20,
-//            //color: Colors.black,
-//            decoration: BoxDecoration(
-//
-//              color: Colors.white,
-//              borderRadius: BorderRadius.only(topLeft: Radius.elliptical(70,40),topRight: Radius.elliptical(70,40)),
-//
-//            ),
-//          ),
+ SizedBox(height: 200,),
 
-        ],
-      );
+             Container(
+               height: 20,
+              //color: Colors.black,
+              decoration: BoxDecoration(
+
+                color: Colors.white,
+                borderRadius: BorderRadius.only(topLeft: Radius.elliptical(70,40),topRight: Radius.elliptical(70,40)),
+
+              ),
+            ),
+
+          ],
+        );
+
 
   }
 
