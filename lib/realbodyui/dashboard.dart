56144446f64e794cf7644ca5819fb7ceb0e3d54c
YasinGujjar <51124860+YasinGujjar@users.noreply.github.com>
2019-12-02 09:27:28 +0500

    new weight page

--- lib/realbodyui/dashboard.dart
+++ lib/realbodyui/dashboard.dart
@@ -7,6 +7,153 @@ class DashBoard extends StatefulWidget {
 class _DashBoardState extends State<DashBoard> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final _tabs = [
+      storeTab(context),
+      Text('Tab2'),
+      Text('Tab3'),
+      Text('Tab4'),
+      Text('Tab5'),
+    ];
+
+    return Scaffold(
+        backgroundColor: Palette.boldTextO,
+        appBar: AppBar(
+          centerTitle: true,
+          elevation: 0,
+          leading: IconButton(
+            onPressed: () {},
+            iconSize: 21,
+            icon: Icon(Icons.fitness_center),
+          ),
+          backgroundColor: Palette.mainPurple,
+          title:
+              Text('Fryo',),
+         
+        ),
+        body: _tabs[_selectedIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+                icon: Icon(Icons.shop),
+                title: Text(
+                  'Store',
+                
+                )),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.shopping_cart),
+                title: Text(
+                  'My Cart',
+                  
+                )),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.favorite),
+                title: Text(
+                  'Favourites',
+                 
+                )),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.person),
+                title: Text(
+                  'Profile',
+                  
+                )),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.settings),
+                title: Text(
+                  'Settings',
+                  
+                ))
+          ],
+          currentIndex: _selectedIndex,
+          type: BottomNavigationBarType.fixed,
+          fixedColor: Colors.green[600],
+          onTap: _onItemTapped,
+        ));
   }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+}
+
+Widget storeTab(BuildContext context) {
+
+  // will pick it up from here
+  // am to start another template
+  
+  return ListView(children: <Widget>[
+    
+  ]);
+}
+
+/* Widget sectionHeader(String headerTitle, {onViewMore}) {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: <Widget>[
+      Container(
+        margin: EdgeInsets.only(left: 15, top: 10),
+        child: Text(headerTitle, style: h4),
+      ),
+      Container(
+        margin: EdgeInsets.only(left: 15, top: 2),
+        child: FlatButton(
+          onPressed: onViewMore,
+          child: Text('View all ›', style: contrastText),
+        ),
+      )
+    ],
+  );
+}
+ */
+// wrap the horizontal listview inside a sizedBox..
+/* Widget headerTopCategories() {
+  return Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    crossAxisAlignment: CrossAxisAlignment.center,
+    children: <Widget>[
+      sectionHeader('All Categories', onViewMore: () {}),
+      SizedBox(
+        height: 130,
+        child: ListView(
+          scrollDirection: Axis.horizontal,
+          shrinkWrap: true,
+          children: <Widget>[
+            headerCategoryItem('Frieds', Fryo.dinner, onPressed: () {}),
+            headerCategoryItem('Fast Food', Fryo.food, onPressed: () {}),
+            headerCategoryItem('Creamery', Fryo.poop, onPressed: () {}),
+            headerCategoryItem('Hot Drinks', Fryo.coffee_cup, onPressed: () {}),
+            headerCategoryItem('Vegetables', Fryo.leaf, onPressed: () {}),
+          ],
+        ),
+      )
+    ],
+  );
+}
+
+Widget headerCategoryItem(String name, IconData icon, {onPressed}) {
+  return Container(
+    margin: EdgeInsets.only(left: 15),
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        Container(
+            margin: EdgeInsets.only(bottom: 10),
+            width: 86,
+            height: 86,
+            child: FloatingActionButton(
+              shape: CircleBorder(),
+              heroTag: name,
+              onPressed: onPressed,
+              backgroundColor: white,
+              child: Icon(icon, size: 35, color: Colors.black87),
+            )),
+        Text(name + ' ›', style: categoryText)
+      ],
+    ),
+  );
+}
 }
